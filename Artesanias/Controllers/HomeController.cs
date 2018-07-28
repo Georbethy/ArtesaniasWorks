8b84f7dab859afcd2bdb024995776e66b3c766f1
Georbethy <rosy.georbethy09@gmail.com>
2018-07-27 23:42:54 -0500

    primer comit

---
+++ Artesanias/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Artesanias.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Artesanias.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Buscar(string id="")
+        {
+            var p = new List<Producto>();
+            //logica
+            using (var bd = new TiendaEntities())
+            {
+                p = bd.Producto.Include("ProductoImagen").Where(x => x.Nombre.Contains(id)).ToList();
+
+            }
+                return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Mens()
+        {
+            ViewBag.Message = "Pagina de artesanias.";
+
+            return View();
+        }
+        
+    }
+
+}
